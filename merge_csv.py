203e5c504537d7ff93f1bcf18cf6a0e2f581c9b5
bkpathk <bkpathk@mtu.edu>
2014-09-08 01:14:01 -0400

    added pandas method

--- merge_csv.py
+++ merge_csv.py
@@ -6,15 +6,15 @@ from collections import OrderedDict
 def merge_file(arg_list):
 	filenames = arg_list[0:-1:2] # extract filename from user input
 	keynames = arg_list[1:-1:2]  # extract share column from user input
-	#filenames = 'data/countries.csv' , 'data/regions.csv'
-	shared_index = OrderedDict()
+	shared_index = OrderedDict() # dictionary to merge files using shared key 
 	column_names = []
 	for filename, keyname in zip(filenames , keynames):
 		with open(filename, "r") as fp: 
 			read_file = csv.DictReader(fp)
 			column_names.extend(read_file.fieldnames)
 			for row in read_file:
-				shared_index.setdefault(row[keyname], {}).update(row)
+				# update the dictionary for each files with shared key
+				shared_index.setdefault(row[keyname], {}).update(row) 
 
 	column_names = list(OrderedDict.fromkeys(column_names))
 	with open("data/merged.csv", "w") as fp:
@@ -27,3 +27,14 @@ def merge_file(arg_list):
 
 if __name__ == '__main__':
 	merge_file(sys.argv[1:])
+
+
+
+#    Alternatively using pandas 
+'''
+import pandas as pd
+df1 = pd.read_csv('data/regions.csv')
+df2 = pd.read_csv('data/countries.csv')
+merged = df1.merge(df2, on="id", how="outer").fillna("")
+merged.to_csv("merged.csv", index=False)
+'''
